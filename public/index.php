b7a087df225e66a50bd68fad886ccabf32ef0d68
iftakharalamrizve <iftakharalam32@gmail.com>
2022-03-16 00:51:13 +0600

    Framework Initialized

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+define('APPLICATION_PATH',substr(realpath(dirname(__FILE__)),0,-6));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+|Composer Autoload loaded automatically generated class loader for
+| our application. We can use this tricks . We only require autoload file .
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+
+
