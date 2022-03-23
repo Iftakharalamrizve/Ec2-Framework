ecb036c8954cfe515412a4ee268b1fdc8b99bca1
iftakharalamrizve <iftakharalam32@gmail.com>
2022-03-24 00:40:40 +0600

    udapte

--- mvc-core/models/User.php
+++ mvc-core/models/User.php
@@ -2,23 +2,11 @@
 
 namespace app\models;
 
-use app\core\Model;
+use app\core\DBModel;
 
-class User extends Model
+class User  extends DBModel
 {
-    protected $attribute = ['first_name','last_name','email','password'];
-    public string $firstname = '';
-    public string $lastname = '';
-    public string $email = '';
-    public string $password = '';
-    public string $passwordConfirm = '';
+    protected $fillable = ['first_name','last_name','email','password'];
+    protected $table = 'users';
 
-    public function attributes(): array
-    {
-        return ['firstname', 'lastname', 'email', 'password'];
-    }
-
-    public function createUser(){
-
-    }
 }
